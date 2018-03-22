1415f59f308e479127fc64ba67eb65b70cd5af31
Peter Anton1 <peteranton1@yahoo.com>
2018-03-22 23:36:01 +0000

    Inital commit

---
+++ src/main/java/toyrobot/ToyRobot.java
@@ -0,0 +1,12 @@
+package toyrobot;
+
+import java.time.LocalTime;
+
+public class ToyRobot {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("The current local time is: " + currentTime);
+        System.out.println("ToyRobot TODO");
+
+    }
+}
